e5e0186ecf2ba09de6f8dabf705bf04cd25f747b
Perpleex <joeleroublard@hotmail.com>
2020-04-07 17:14:19 +0200

    Update tts.js
    not work

--- www/tts.js
+++ www/tts.js
@@ -20,30 +20,30 @@ exports.speak = function (text) {
             options = text;
         }
 
-        cordova.exec(resolve, reject(err), 'TTS', 'speak', [options]);
+        cordova.exec(resolve, reject, 'TTS', 'speak', [options]);
     });
 };
 
 exports.stop = function() {
     return new Promise(function (resolve, reject) {
-        cordova.exec(resolve, reject(err), 'TTS', 'stop', []);
+        cordova.exec(resolve, reject, 'TTS', 'stop', []);
     });
 };
 
 exports.checkLanguage = function() {
     return new Promise(function (resolve, reject) {
-        cordova.exec(resolve, reject(err), 'TTS', 'checkLanguage', []);
+        cordova.exec(resolve, reject, 'TTS', 'checkLanguage', []);
     });
 };
 
 exports.getVoices = function(text) {
     return new Promise(function (resolve, reject) {
-        cordova.exec(resolve, reject(err), 'TTS', 'getVoices', [text]);
+        cordova.exec(resolve, reject, 'TTS', 'getVoices', [text]);
     });
 };
 
 exports.openInstallTts = function() {
     return new Promise(function (resolve, reject) {
-        cordova.exec(resolve, reject(err), 'TTS', 'openInstallTts', []);
+        cordova.exec(resolve, reject, 'TTS', 'openInstallTts', []);
     });
 };
